717ca8cc6ad237683df8d024ad0c340e859628a5
a810434655 <woshiwuxingxing@qq.com>
2018-11-29 17:38:36 +0800

    2018-11-29更新

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 "use strict"
 
 var Koa=require("koa");
-var sha1=require('sha1');
+var wechat=require("./wechat/g");
 var config={
     wechat:{
         appID:"wx1349e9c2e66242e7",
@@ -9,25 +9,8 @@ var config={
         token:"wupeng"
     }
 }
-var app = new Koa()
+var app = new Koa();
+app.use(wechat(config.wechat));
 
-app.use(function *(next) {
-    console.log(this.query)
-    var token=config.wechat.token;
-    var signature=this.query.signature;
-    var nonce=this.query.nonce;
-    var timestamp=this.query.timestamp;
-    var echostr=this.query.echostr;
-    var str=[token,timestamp,nonce].sort().join("")
-    var sha=sha1(str);
-    if(sha===signature){
-        console.log("进入"+echostr);
-        this.body=echostr+''
-    }else{
-        console.log(sha);
-        console.log("失败"+token);
-        this.body="wrong"
-    }
-})
 app.listen(3200);
 console.log("OK：3200");
